9747dadf43455a29a063b941ae214892e7851139
sgt99 <gabrielsegatti@hotmail.com>
2017-06-13 17:36:06 -0300

    Update CrawlerGetNomes.java

--- CrawlerGetNomes.java
+++ CrawlerGetNomes.java
@@ -8,57 +8,51 @@ import org.jsoup.nodes.Node;
 import org.jsoup.select.Elements;
 
 public class CrawlerGetNomes extends CrawlerIndividualStats {
-
-	public String[] extrairNomeEId(Element elemento, String url){			//Ao ler um elemento que virá a ser um jogador, seus dados aqui são inseridos, retornando												//SUA id para a hash e seu nome
+	
+	public String[] extrairNomeEId(Element elemento){
+		int cont = 36;		
 		String[] idENome = new String[2];
 
-		for(Node jogadorNode : elemento.childNodes()){
-			for(Node info: jogadorNode.childNodes()){	//retira o nome
-				if(!(info.childNodes().isEmpty())){
-					int cont = 36;
-	
-					while(info.attr("href").toString().charAt(cont) != '/')
-						cont++;
-					
-					idENome[0] = info.attr("href").substring(36, cont).toString();
-					idENome[1] = info.childNode(0).toString();
-					
-					try{
-						Document doc = null;
-						/*Document doc = null;
-						System.out.println(info.attr("href").toString());
-						doc = Jsoup.connect(info.attr("href").toString()).get();
-						
-						Elements classe = doc.getElementsByClass("main-headshot");
-						Element foto = classe.get(0);
-						System.out.println(foto.child(0).attr("src").toString());*/
-						
-
-						doc = Jsoup.connect(info.attr("href").toString()).get();
-						Elements classe = pagina.getElementsByClass("main-headshot");
-						if(!classe.isEmpty()){
-							Element foto = classe.get(0);
-							idENome[1] += "\n" + (foto.child(0).attr("src").toString());
-						}
-						if(classe.toString().isEmpty()){
-							classe = pagina.getElementsByClass("main-headshot-65 floatleft");
-							if(classe.toString().isEmpty()){
-								throw new IOException();
-							}
-							Element foto = classe.get(0);
-							idENome[1] += "\n" + (foto.child(0).attr("src").toString());
-						}
-						
-					}catch(IOException e){
-						System.out.println("Foto não existente");
-					}
-
-					//System.out.println(idENome[1] + "\n" + idENome[0]);
-				}
-			}
-		
-		}
+		Node jogadorNode = elemento.childNode(1);
+				
+		while(jogadorNode.childNode(0).attr("href").toString().charAt(cont) != '/')
+			cont++;
+			
+		idENome[0] = jogadorNode.childNode(0).attr("href").substring(36, cont).toString();
+		idENome[1] = jogadorNode.childNode(0).childNode(0).toString();				
+			
 		return idENome;
+	}
+	
+	public void getFoto(String idENome[], Element elemento){
+		try{
+			Document doc = null;
+			Node jogadorNode = elemento.childNode(1);
+			doc = Jsoup.connect(jogadorNode.childNode(0).attr("href").toString()).get();
+			Elements classe = doc.getElementsByClass("main-headshot");
+		
+			if(!classe.isEmpty()){
+				Element foto = classe.get(0);
+				String link = foto.child(0).attr("src").toString();
+				int x = link.indexOf("&");
+				link = link.substring(0, x);
+				idENome[1] += "\n" + (link);
+			}
+			
+			if(classe.toString().isEmpty()){
+				classe = doc.getElementsByClass("main-headshot-65 floatleft");
+				if(classe.toString().isEmpty()){
+					throw new IOException();
+				}
+				Element foto = classe.get(0);
+				idENome[1] += "\n" + (foto.child(0).attr("src").toString());
+			}
+					
+		}catch(IOException e){
+			idENome[1] += "\n" + null;
+			System.out.println("Foto não existente");
+		}
+		System.out.println(idENome[1] + "\n" + idENome[0]);
 	}
 
 	public void iterarColunasPares(String url, HashMap<String, String> hashJogadores) throws IOException{
@@ -70,10 +64,12 @@ public class CrawlerGetNomes extends CrawlerIndividualStats {
 				Elements colunasPares = pagina.select(".tablehead .evenRow");
 			
 				for(Element elemento : colunasPares){
-					idENome = extrairNomeEId(elemento, url);
-				
-					if(! (hashJogadores.containsKey(idENome[0])) )	//Se a chave do jogador, retornada por (extrair), não existir na hash
+					idENome = extrairNomeEId(elemento);
+					
+					if(! (hashJogadores.containsKey(idENome[0])) ){	//Se a chave do jogador, retornada por (extrair), não existir na hash
+						getFoto(idENome, elemento);
 						hashJogadores.put(idENome[0], idENome[1]);
+					}
 				}
 			}catch(IOException e){
 				System.out.println("Erro de conexão: " + e.toString());
@@ -90,10 +86,12 @@ public class CrawlerGetNomes extends CrawlerIndividualStats {
 				Elements colunasImpares = pagina.select(".tablehead .oddRow");
 			
 				for(Element elemento : colunasImpares){
-					idENome = extrairNomeEId(elemento, url);		
-			
-					if(! (hashJogadores.containsKey(idENome[0])) )	//Se a chave do jogador, retornada por (extrair), não existir na hash
+					idENome = extrairNomeEId(elemento);		
+					
+					if(! (hashJogadores.containsKey(idENome[0])) ){	//Se a chave do jogador, retornada por (extrair), não existir na hash
+						getFoto(idENome, elemento);
 						hashJogadores.put(idENome[0], idENome[1]);
+					}
 				}
 			}catch(IOException e){
 				System.out.println("Erro de conexão: " + e.toString());
